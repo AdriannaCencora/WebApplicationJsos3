e867ba015260ce62028988c55f680582cf825b77
Adrianna Cencora <adrianna.cencora@gmail.com>
2019-01-27 18:34:46 +0100

    Add fully functional add, remove and find methods

--- src/systemzapisowy/Application.java
+++ src/systemzapisowy/Application.java
@@ -40,87 +40,86 @@ private static PreparedStatement ps = null;
         students = new ArrayList();
     }
 
-    public String addCourse(String[] data) {
-        Factory fabryka = new Factory();
-        Course course = fabryka.createCourse(data);
-        Course foundCourse = findCourse(course);
-        if (foundCourse == null) {
-            courses.add(course);
-            String info = course.toString();
-            return info;
-        }
-        return null;
-}
+//    public String addCourse(String[] data) {
+//        Factory fabryka = new Factory();
+//        Course course = fabryka.createCourse(data);
+//        Course foundCourse = findCourse(course);
+//        if (foundCourse == null) {
+//            courses.add(course);
+//            String info = course.toString();
+//            return info;
+//        }
+//        return null;
+//}
+//    
+//public String addUser(String[] data) {
+//        Factory factory = new Factory();
+//        User user = factory.createUser(data);
+//        // foundStudent = findStudentByIndex(student);
+//       // if (foundStudent == null) {
+//         
+//            addUserToDatabase(user);
+//
+//           // String info = student.toString();
+//    //        return null;
+//      //  }
+//        return null;
+//    }
+//    
     
-public String addUser(String[] data) {
-        Factory factory = new Factory();
-        User user = factory.createUser(data);
-        // foundStudent = findStudent(student);
-       // if (foundStudent == null) {
-         
-            addUserToDatabase(user);
+//public void addUserToDatabase (User user) {
+//      System.out.println(user.toString());
+//    
+//            try {
+//            ps  = conn.prepareStatement("INSERT INTO Users"
+//                    + " VALUES (?, ?, ?, ?)");
+//            ps.setInt(1, Integer.parseInt(user.getID()));
+//            ps.setString(2, user.getName());
+//            ps.setString(3, user.getSurname());
+//            
+//            SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+//            Date parsed = null;
+//            try {
+//                System.out.println(user.getBirthDate());
+//                   parsed = format.parse(user.getBirthDate());
+//            }  catch (ParseException e1) {
+//                        e1.printStackTrace();
+//            }
+//            java.sql.Date sql = new java.sql.Date(parsed.getTime());
+//            ps.setDate(4, sql);
+//            
+//            ps.executeUpdate();
+//            ps.close();
+//            }
+//            catch (SQLException ex) {
+//
+//        ex.printStackTrace();
+//
+//    }
+//
+//    
+//}
 
-           // String info = student.toString();
-    //        return null;
-      //  }
-        return null;
-    }
-    
-    
-public void addUserToDatabase (User user) {
-      System.out.println(user.toString());
-    
-            try {
-            ps  = conn.prepareStatement("INSERT INTO Users"
-                    + " VALUES (?, ?, ?, ?)");
-            ps.setInt(1, Integer.parseInt(user.getID()));
-            ps.setString(2, user.getName());
-            ps.setString(3, user.getSurname());
-            
-            SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
-            Date parsed = null;
-            try {
-                System.out.println(user.getBirthDate());
-                   parsed = format.parse(user.getBirthDate());
-            }  catch (ParseException e1) {
-                        e1.printStackTrace();
-            }
-            java.sql.Date sql = new java.sql.Date(parsed.getTime());
-            ps.setDate(4, sql);
-            
-            ps.executeUpdate();
-            ps.close();
-            }
-            catch (SQLException ex) {
-
-        ex.printStackTrace();
-
-    }
-
-    
-}
-
     
     
     
     
     
-public String addStudent(String[] data) {
+public void addStudent(String[] data) {
         Factory factory = new Factory();
         Student student = factory.createStudent(data);
-        Student foundStudent = findStudent(student);
-        if (foundStudent == null) {
-         
-          addStudentToDatabase(student);
-
-            String info = student.toString();
-            return null;
+        
+        if(addStudentToDatabase(student)) {
+            System.out.println("Adding student succed.");
         }
-        return null;
+        else 
+            System.out.println("Adding student failed.");
+        
+    
     }
 
 
-public void addStudentToDatabase(Student student) {
+public boolean addStudentToDatabase(Student student) {
     
         System.out.println(student.toString());
     
@@ -157,7 +156,7 @@ public void addStudentToDatabase(Student student) {
              rs = stmt.executeQuery(query1);
              
             if (rs.next()) {
-                ps.setString(2, rs.getString("userID") );
+                ps.setInt(2, Integer.parseInt(rs.getString("userID") ));
             }
              
              
@@ -171,58 +170,60 @@ public void addStudentToDatabase(Student student) {
             }
              ps.setInt(4, Integer.parseInt(student.getSemester()));
 
-             ps.executeUpdate();
-             ps.close();
+             
+             int succeed = ps.executeUpdate();
+             if (succeed == 1) {
+                 return true;
+             }
              
         } catch (SQLException ex) {
 
         ex.printStackTrace();
 
     }
+            return false;
 }
     
 
+    public void removeStudent(int indexNumber) {
+        if(removeStudentFromDatabase(indexNumber)) {
+            System.out.println("Removing succed.");
+        }
+        else 
+            System.out.println("Removing failed.");
+
+    }
+
             
-            
-            
-//            public boolean deleteUser(int userID) {
-//       
-//        try {
-//            Statement stmt = conn.createStatement();
-//            int i = stmt.executeUpdate("DELETE FROM user WHERE userID=" + userID);
-//            if(i == 1) {
-//                return true;
-//            }
-//        } catch (SQLException ex) {
-//            ex.printStackTrace();
-//        }
-//            return false;
-//      }
+           
     
-    public boolean removeStudentFromDatabase(int studentID) {
+    public boolean removeStudentFromDatabase(int studentIndex) {
        
         try {
-            Statement stmt = conn.createStatement();
+            stmt = conn.createStatement();
+            Statement stmt1 = conn.createStatement();
+            String queryToSelect = "SELECT userID FROM Students WHERE indexNum = " + studentIndex;
+            rs = stmt1.executeQuery(queryToSelect);
 
-            String query = "SELECT userID FROM Students WHERE indexNum = " + studentID;
-            
-                         ResultSet rs = stmt.executeQuery(query);
-
-            
-            int i = stmt.executeUpdate("DELETE FROM Students WHERE indexNum = " + studentID);
-            if(i == 1) {
+            //Removing student from groupRecords table.
+            int k = stmt.executeUpdate("DELETE FROM GroupRecords WHERE studentIndex = " + studentIndex );
+                
+            int i = stmt.executeUpdate("DELETE FROM Students WHERE indexNum = " + studentIndex);
+            if(i == 1 && k == 1) {
                 try {
-                Statement stmt2 = conn.createStatement();
                 if (rs.next()) {
-                int j = stmt2.executeUpdate("DELETE FROM Users WHERE userID = " + rs.getInt("userID"));
+           
+                int j = stmt1.executeUpdate("DELETE FROM Users WHERE userID = " + rs.getInt("userID"));
+                
+                        if (j == 1) 
+                            return true; 
+
                 }
                 } catch (SQLException ex) {
                     ex.printStackTrace();
                 }
-                  return true; 
             }
-            
-            
+              
                 
         } catch (SQLException ex) {
             ex.printStackTrace();
@@ -231,30 +232,23 @@ public void addStudentToDatabase(Student student) {
       }
             
             
-            
 
             
-            
-            
-            
-            
-            
-            
 
-    public String addGroup(String[] data, Course course) {
-        StudyGroup studyGroup = course.addGroup(data);
-        if (studyGroup == null) {
-            course.getGroups().add(studyGroup);
-            String info = course.toString();
-            return info;
-        }
-        return null;
-    }
+//    public String addGroup(String[] data, Course course) {
+//        StudyGroup studyGroup = course.addGroup(data);
+//        if (studyGroup == null) {
+//            course.getGroups().add(studyGroup);
+//            String info = course.toString();
+//            return info;
+//        }
+//        return null;
+//    }
 
-    public void removeCourse(String courseCode) {
-        // TODO - implement Aplikacja.usunKurs
-        throw new UnsupportedOperationException();
-    }
+//    public void removeCourse(String courseCode) {
+//        // TODO - implement Aplikacja.usunKurs
+//        throw new UnsupportedOperationException();
+//    }
     
      public Course findCourse(String courseCode) {
         for (int i = 0; i < courses.size(); i++) {
@@ -265,47 +259,89 @@ public void addStudentToDatabase(Student student) {
         return null;
     }
 
-    public Course findCourse(Course course) {
-        int idx;
-        if ((idx = courses.indexOf(course)) != -1) {
-            course = courses.get(idx);
-            return course;
-        }
-        return null;
-    }
+//    public Course findCourse(Course course) {
+//        int idx;
+//        if ((idx = courses.indexOf(course)) != -1) {
+//            course = courses.get(idx);
+//            return course;
+//        }
+//        return null;
+//    }
 
-    public Student findStudent(String indexNumber) {
-        for (int i = 0; i < students.size(); i++) {
-            if (students.get(i).getIndexNumber().equals(indexNumber)) {
-                return students.get(i);
+     
+     
+    public Student findStudentByIndex(String indexNumber) {
+        Student student = new Student();
+        
+        try {
+
+            ResultSet resultSetUser;
+            ResultSet resultSetStudent;
+            
+             Statement stmtUser = conn.createStatement();
+
+             Statement stmtStudent = conn.createStatement();
+
+            
+            stmt = conn.createStatement();
+            String queryToSelectID = "SELECT userID FROM Students WHERE indexNum = " + indexNumber;
+            rs = stmt.executeQuery(queryToSelectID);
+            
+            
+            if (rs.next()) {
+           
+                resultSetUser = stmtUser.executeQuery("SELECT * FROM Users WHERE userID = " + rs.getInt("userID"));
+                
+                if (resultSetUser.next()) {
+                    student.setID(resultSetUser.getString("userID"));
+                    student.setName(resultSetUser.getString("firstName"));
+                    student.setSurname(resultSetUser.getString("lastName"));
+                    student.setBirthDate(resultSetUser.getString("birthDate"));
+
+                }
+             }
+            
+            resultSetStudent = stmtStudent.executeQuery("SELECT *  FROM Students WHERE indexNum  = " + indexNumber );
+            
+            
+            
+            if(resultSetStudent.next())
+
+            {
+
+                student.setIndexNumber(resultSetStudent.getString("indexNum") );
+                student.setFieldOfStudy(resultSetStudent.getString("fieldOfStudy") );
+                student.setSemester(resultSetStudent.getString("semester") );
+
+
             }
+
+                return student;
+
+        } catch (SQLException ex) {
+
+            ex.printStackTrace();
+
         }
-        return null;
+
+    return null;
     }
     
-     public Student findStudent(Student student) {
-        int idx;
-        if ((idx = students.indexOf(student)) != -1) {
-            student = students.get(idx);
-            return student;
-        }
-        return null;
-    }
+//     public Student findStudentByIndex(Student student) {
+//        int idx;
+//        if ((idx = students.indexOf(student)) != -1) {
+//            student = students.get(idx);
+//            return student;
+//        }
+//        return null;
+//    }
+//
 
-    public void removeStudent(int indexNumber) {
-        // TODO - implement Aplikacja.usunStudenta
-        if(removeStudentFromDatabase(indexNumber)) {
-            System.out.println("Removing succed.");
-        }
-        else 
-            System.out.println("Removing succed.");
-
-    }
     
     
     public void enrollStudent(String groupCode, String indexNumber, String courseCode) {
 
-        Student student = findStudent(indexNumber);
+        Student student = findStudentByIndex(indexNumber);
 
         if (student == null) {
             System.out.println("Nie znaleziono studenta!");
@@ -343,20 +379,22 @@ public void addStudentToDatabase(Student student) {
     
     Application app = new Application();
     Factory factory = new Factory();
-    
-    
+
         //String[] daneUsera1 = {"111226", "Gal", "Anonim", "2001-05-05"}; 
-        String[] daneStudenta1 = {"100101", "Adrianna", "Cencora", "1995-05-05", "218403", "Informatyka", "5"};
+        String[] daneStudenta1 = {"50000", "Adrianna", "Cencora", "1995-05-05", "218403", "Informatyka", "5"};
         String[] daneKursu1 = {"HHH234", "Inżynieria Oprogramowania", "Laboratorium", "Informatyka", "5"};
         String[] daneGrupy1 = {"BBB123", "mgr. inż. Adam Małysz", "25", "15", "Środa"};
 
-        //app.addStudent(daneStudenta1);
-
-       app.removeStudent(218403);
+     //app.addStudent(daneStudenta1);
+    
+    Student student = app.findStudentByIndex("218403");
+    
+    System.out.println(student.toString());
+    //  app.removeStudent(218400);
 //        System.out.println(app.addCourse(daneKursu1));
 //        System.out.println(app.addGroup(daneGrupy1, app.courses.get(0)));
 //
-//        Student student = app.findStudent(daneStudenta1[0]);
+//        Student student = app.findStudentByIndex(daneStudenta1[0]);
 //        app.enrollStudent(daneGrupy1[0], daneStudenta1[4], daneKursu1[0]);
     
     ConnectionMeneger.closeConnection();
